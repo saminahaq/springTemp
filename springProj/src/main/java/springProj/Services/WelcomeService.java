2bb28b25ee562d1021644cb3d15d9548e07508fb
samina <saminanaseem@ymail.com>
2019-01-31 13:58:37 -0800

    first commit

---
+++ springProj/src/main/java/springProj/Services/WelcomeService.java
@@ -0,0 +1,12 @@
+package springProj.Services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WelcomeService {
+	public String retrieveWelcomeMessage() {
+		//Complex Method
+		return "Good Morning updated";
+	}
+
+}
